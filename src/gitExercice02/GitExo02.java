9a5134ab0d616548475391dd6c8dcdbae396ece9
Gilles <quodbach.gilles@gmail.com>
2024-01-04 15:42:45 +0100

    corrections

--- src/gitExercice02/GitExo02.java
+++ src/gitExercice02/GitExo02.java
@@ -13,50 +13,55 @@ public class GitExo02 {
 		// Nombre allumettes départ
 
 		int startMatchesNumber = 21;
-		
-		int player1MatchesNumber = startMatchesNumber;
-		int player2MatchesNumber = startMatchesNumber;
-		
 
 		String player1Name = "";
 		String player2Name = "";
 		int currentPlayer = 0;
-		int winner = 0;
-		int howManyMatches = 0;
+
+		String howManyMatches = "";
 		boolean isValidNumber = true;
 
 		System.out.println("Bienvenue au jeu du NIM");
 
 		System.out.println("Entrer nom joueur 1");
 		player1Name = scan.nextLine();
-		
-		
+
 		System.out.println("Entrer nom joueur 2");
 		player2Name = scan.nextLine();
 
-
 		System.out.println("Quel joueur commence la partie ? 1 ou 2");
 		currentPlayer = Integer.parseInt(scan.nextLine());
-			System.out.println("A " + (currentPlayer == 1 ? player1Name : player2Name) + " de commencer la partie");
-			System.out.println("Le nombre d'alumette total est de: " + startMatchesNumber );
-			while (startMatchesNumber > 0) {
-				while (isValidNumber) {
-					System.out.println("A " + (currentPlayer == 1 ? player1Name : player2Name) + " de jouer");
-					System.out.println("Combien d'allumette voulez vous retirez ? (1 à 4)");
-					howManyMatches = Integer.parseInt(scan.nextLine());
-					if ((howManyMatches < 1) || (howManyMatches > 4)) {
-						System.out.println("Veuillez choisir un nombre entre 1 et 4");
-					} else {
+		while (currentPlayer != 1 && currentPlayer != 2) {
+			System.out.println("Chiffre incorrect ! Quel joueur commence la partie ? 1 ou 2");
+			currentPlayer = Integer.parseInt(scan.nextLine());
+		}
+		System.out.println("A " + (currentPlayer == 1 ? player1Name : player2Name) + " de commencer la partie");
+		System.out.println("Le nombre d'alumette total est de: " + startMatchesNumber);
+		while (startMatchesNumber > 0) {
+			while (isValidNumber) {
+				System.out.println("A " + (currentPlayer == 1 ? player1Name : player2Name) + " de jouer");
+				System.out.println("Combien d'allumette voulez vous retirez ? (1 à 4)");
+				howManyMatches = scan.nextLine();
+				while (howManyMatches != "1" || howManyMatches != "2" ||howManyMatches != "3" ||howManyMatches != "4") {
+					System.out.println("Saisie incorrect !Veuillez entrer un chiffre entre 1 et 4.");
+					howManyMatches = scan.nextLine();
+				}
+				int playerNumberOfMatches = Integer.parseInt(howManyMatches);
+				if (playerNumberOfMatches >= startMatchesNumber) {
+					System.out.println("Veuillez choisir un nombre inférieur à " + startMatchesNumber);
+				} else if ((playerNumberOfMatches < 1) || (playerNumberOfMatches > 4)) {
+					System.out.println("Veuillez choisir un nombre entre 1 et 4");
+				} else {
 
-						startMatchesNumber = startMatchesNumber - howManyMatches;
-						System.out.println("Allumette restante: " + startMatchesNumber);
-						if (startMatchesNumber <= 0) {
-							System.out.println((currentPlayer == 1 ? player1Name : player2Name) + " à perdu !");
-							break;
-						}
-						currentPlayer = (currentPlayer == 1) ? 2 : 1;
+					startMatchesNumber = startMatchesNumber - playerNumberOfMatches;
+					System.out.println("Allumette restante: " + startMatchesNumber);
+					if (startMatchesNumber <= 0) {
+						System.out.println((currentPlayer == 1 ? player1Name : player2Name) + " à perdu !");
+						break;
 					}
+					currentPlayer = (currentPlayer == 1) ? 2 : 1;
 				}
 			}
+		}
 	}
 }
